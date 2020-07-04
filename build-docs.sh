1659fa08dbefb6f3a7030506c1031b55a8251bd1
Austin Hicks <camlorn@camlorn.net>
2020-07-04 16:22:45 -0700

    Move to an organization; build docs as the org's github pages

--- build-docs.sh
+++ build-docs.sh
@@ -3,4 +3,9 @@ set -ex
 cd manual
 mdbook build
 cd ..
-cp -r manual/book/html docs
+rm -rf docs/*
+cp -r manual/book/html/ docs/
+cd docs
+git add -A
+git commit -m "Build site at $(date)" --allow-empty
+git push
