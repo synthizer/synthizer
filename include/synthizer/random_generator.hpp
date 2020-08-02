ae3cc89be7b13b34cb1737c10f6a8ea35bf4081a
Austin Hicks <ahicks@ahicks.io>
2020-08-02 13:57:35 -0700

    Bring in Xorshiro and write some random number generators.
    We'll want to benchmark these at some point.

---
+++ include/synthizer/random_generator.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "synthizer/xoshiro.hpp"
+
+#include <array>
+#include <atomic>
+#include <cassert>
+#include <random>
+#include <cstdint>
+#include <cstring>
+
+namespace synthizer {
+
+std::array<std::uint64_t, 4>
+static inline makeXoshiroSeed() {
+	static std::atomic<int> seed_start = 0;
+	int start = seed_start.fetch_add(8, std::memory_order_relaxed);
+	std::array<std::uint32_t, 8> seed32;
+	std::seed_seq gen{start, start+1, start+2, start+3, start+4, start+5, start+6, start+6};
+	gen.generate(seed32.begin(), seed32.end());
+	return { (std::uint64_t(seed32[0])<<32)|seed32[1], (std::uint64_t(seed32[2])<<32)|seed32[3],
+	  (std::uint64_t(seed32[4])<<32)|seed32[5], (std::uint64_t(seed32[6])<<32)|seed32[7]};
+}
+
+/*
+ * Converts a significand, a binary number with 23 bits, to a float between -1 and 1.
+ * */
+static inline float significandToFloat(unsigned int significand) {
+	/* Positive. */
+	const std::uint32_t sign = 0x00000000;
+	/* Exponent = 1. */
+	const std::uint32_t exponent = 0x40000000;
+	float out;
+	std::uint32_t in = sign | exponent | significand;
+	std::memcpy(&out, &in, sizeof(float));
+	/* Our float is between 2 and 4. We want between -1 and 1. */
+	return out - 3.0f;
+}
+
+/*
+ * Knows how to generate a variety of types of audio sample.  All real results are between -1 and 1.
+ * */
+class RandomGenerator {
+	public:
+	RandomGenerator(): gen(makeXoshiroSeed()) {
+	}
+
+
+	float generateFloat() {
+		std::uint64_t num = this->gen.next();
+		return significandToFloat(num & ((2<<22)-1));
+	}
+
+	/*
+	 * Generate 4 floats, and write them to the specified output references.
+	 * 
+	 * These floats are what you'd get if you converted 16-bit signed samples to floats, i.e. less precise than generateFloat but
+	 * still good enough for audio.  This is much faster than generating 1 float 4 times, because
+	 * we can get 4 floats from one run of a 64-bit random number generator, as long as we don't mind
+	 * the limitation. 
+	 * 
+	 * We don't use a tuple here because of debug builds; we want this to be as fast
+	 * as we can manage.
+	 * */
+	void generateFloat4(float &o1, float &o2, float &o3, float &o4) {
+		std::uint64_t random = this->gen.next();
+		std::uint16_t i1, i2, i3, i4;
+		const std::uint16_t bit16 = 0xffff;
+		i1 = random & bit16;
+		i2 = (random >> 16) & bit16;
+		i3 = (random >> 32) & bit16;
+		i4 = (random>>48) & bit16;
+		o1 = 1.0f - (i1 / 32768.0f);
+		o2 = 1.0f - (i2 / 32768.0f);
+		o3 = 1.0f - (i3 / 32768.0f);
+		o4 = 1.0f - (i4 / 32768.0f);
+	}
+
+	private:
+	xoshiro::Xoshiro256PlusPlus gen;
+};
+
+}
---
+++ include/synthizer/xoshiro.hpp
@@ -0,0 +1,127 @@
+
+#pragma once
+
+/*
+ * The following is Xoshiro256=+ modified to be in a class
+ * and put in the synthizer namespace.
+ * 
+ * Most comments are the original.
+ * */
+
+
+/*  Written in 2019 by David Blackman and Sebastiano Vigna (vigna@acm.org)
+
+To the extent possible under law, the author has dedicated all copyright
+and related and neighboring rights to this software to the public domain
+worldwide. This software is distributed without any warranty.
+
+See <http://creativecommons.org/publicdomain/zero/1.0/>. */
+
+#include <array>
+#include <cstdint>
+
+namespace synthizer::xoshiro {
+
+/* This is xoshiro256++ 1.0, one of our all-purpose, rock-solid generators.
+   It has excellent (sub-ns) speed, a state (256 bits) that is large
+   enough for any parallel application, and it passes all tests we are
+   aware of.
+
+   For generating just floating-point numbers, xoshiro256+ is even faster.
+
+   The state must be seeded so that it is not everywhere zero. If you have
+   a 64-bit seed, we suggest to seed a splitmix64 generator and use its
+   output to fill s. */
+
+static inline std::uint64_t rotl(const uint64_t x, int k) {
+	return (x << k) | (x >> (64 - k));
+}
+
+class Xoshiro256PlusPlus {
+	private:
+	std::uint64_t s[4];
+
+	public:
+	Xoshiro256PlusPlus(std::array<std::uint64_t, 4> seed) {
+		this->s[0] = seed[0];
+		this->s[1] = seed[1];
+		this->s[2] = seed[2];
+		this->s[3] = seed[3];
+	}
+
+	std::uint64_t next() {
+		const std::uint64_t result = rotl(s[0] + s[3], 23) + s[0];
+
+		const std::uint64_t t = s[1] << 17;
+
+		s[2] ^= s[0];
+		s[3] ^= s[1];
+		s[1] ^= s[2];
+		s[0] ^= s[3];
+
+		s[2] ^= t;
+
+		s[3] = rotl(s[3], 45);
+
+		return result;
+	}
+
+
+	/* This is the jump function for the generator. It is equivalent
+	to 2^128 calls to next(); it can be used to generate 2^128
+	non-overlapping subsequences for parallel computations. */
+	void jump(void) {
+		static const std::uint64_t JUMP[] = { 0x180ec6d33cfd0aba, 0xd5a61266f0c9392c, 0xa9582618e03fc9aa, 0x39abdc4529b1661c };
+
+		std::uint64_t s0 = 0;
+		std::uint64_t s1 = 0;
+		std::uint64_t s2 = 0;
+		std::uint64_t s3 = 0;
+		for(int i = 0; i < sizeof JUMP / sizeof *JUMP; i++)
+			for(int b = 0; b < 64; b++) {
+				if (JUMP[i] & (std::uint64_t(1)) << b) {
+					s0 ^= s[0];
+					s1 ^= s[1];
+					s2 ^= s[2];
+					s3 ^= s[3];
+				}
+				next();	
+			}
+			
+		s[0] = s0;
+		s[1] = s1;
+		s[2] = s2;
+		s[3] = s3;
+	}
+
+
+	/* This is the long-jump function for the generator. It is equivalent to
+	2^192 calls to next(); it can be used to generate 2^64 starting points,
+	from each of which jump() will generate 2^64 non-overlapping
+	subsequences for parallel distributed computations. */
+	void long_jump(void) {
+		static const std::uint64_t LONG_JUMP[] = { 0x76e15d3efefdcbbf, 0xc5004e441c522fb3, 0x77710069854ee241, 0x39109bb02acbe635 };
+
+		std::uint64_t s0 = 0;
+		std::uint64_t s1 = 0;
+		std::uint64_t s2 = 0;
+		std::uint64_t s3 = 0;
+		for(int i = 0; i < sizeof LONG_JUMP / sizeof *LONG_JUMP; i++)
+			for(int b = 0; b < 64; b++) {
+				if (LONG_JUMP[i] & std::uint64_t(1) << b) {
+					s0 ^= s[0];
+					s1 ^= s[1];
+					s2 ^= s[2];
+					s3 ^= s[3];
+				}
+				next();	
+			}
+			
+		s[0] = s0;
+		s[1] = s1;
+		s[2] = s2;
+		s[3] = s3;
+	}
+};
+
+}
