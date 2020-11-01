06eb6befef15014bd603b6cabe4b82f779607e21
Austin Hicks <ahicks@ahicks.io>
2020-10-31 19:23:39 -0700

    Support writing arbitrary C arrays from Python and use this to include an array of primes
    We'll use the primes in a bit for reverb, which wants mutually prime delay
    lengths.

--- data_processor/main.py
+++ data_processor/main.py
@@ -1,4 +1,20 @@
+import pyprimes
+
+import array_writer
 import hrtf_writer
 import mit
 
 hrtf_writer.write_hrtf_data(mit.hrtf_data)
+
+def primes(limit):
+    """Get all primes including the first prime above the specified threshold."""
+    for i in pyprimes.primes():
+        if i > limit:
+            yield i
+            return
+        yield i
+
+arrays = array_writer.ArrayWriter()
+#We primarily use primes for delay line lengths, and our default sampling rate is 44100. Give us at least 5 seconds worth.
+arrays.add_array("primes", "unsigned int", primes(44100 * 5))
+arrays.write_arrays()
