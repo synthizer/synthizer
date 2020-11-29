106cd17e889490ef32bf6bd9203bd64ea1546297
Austin Hicks <ahicks@ahicks.io>
2020-11-29 11:10:15 -0800

    Move EchoEffect implementation to the header.
    More preliminary work in preparation for refactoring for #32.

--- include/synthizer/effects/echo.hpp
+++ include/synthizer/effects/echo.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "synthizer/block_delay_line.hpp"
+#include "synthizer/channel_mixing.hpp"
 #include "synthizer/config.hpp"
 #include "synthizer/effects/base_effect.hpp"
 #include "synthizer/effects/global_effect.hpp"
@@ -8,6 +9,9 @@
 #include "synthizer/types.hpp"
 
 #include "concurrentqueue.h"
+
+#include <array>
+#include <utility>
 
 namespace synthizer {
 
@@ -69,5 +73,81 @@ class GlobalEchoEffect: public GlobalEffect<EchoEffect> {
 
 	#include "synthizer/property_methods.hpp"
 };
+
+template<bool FADE_IN, bool ADD>
+void EchoEffect::runEffectInternal(float *output, float gain) {
+	this->line.runReadLoop(this->max_delay_tap, [&](unsigned int i, auto &reader) {
+		float acc_l = 0.0f;
+		float acc_r = 0.0f;
+		for (auto &t: this->taps) {
+			acc_l += reader.read(0, t.config.delay) * t.config.gain_l;
+			acc_r += reader.read(1, t.config.delay) * t.config.gain_r;
+		}
+		if (FADE_IN) {
+			float g = i / (float)config::BLOCK_SIZE;
+			acc_l *= g;
+			acc_r *= g;
+		}
+		acc_l *= gain;
+		acc_r *= gain;
+		if (ADD) {
+			output[i * 2] += acc_l;
+			output[i * 2 + 1] += acc_r;
+		} else {
+			output[i * 2] = acc_l;
+			output[i * 2 + 1] = acc_r;
+		}
+	});
+}
+
+void EchoEffect::runEffect(unsigned int time_in_blocks, unsigned int input_channels, float *input, unsigned int output_channels, float *output, float gain) {
+	thread_local std::array<float, config::BLOCK_SIZE * 2> working_buf;
+
+	auto *buffer = this->line.getNextBlock();
+	std::fill(buffer, buffer + config::BLOCK_SIZE * 2, 0.0f);
+	/* Mix data to stereo. */
+	mixChannels(config::BLOCK_SIZE, input, input_channels, buffer, 2);
+
+	/* maybe apply the new config. */
+	deferred_vector<EchoTapConfig> new_config;
+	bool will_crossfade = this->pending_configs.try_dequeue(new_config);
+	/* Drain any stale values. When this is moved to a proper box, this will be handled for us. */
+	while (this->pending_configs.try_dequeue(new_config));
+	if (will_crossfade) {
+		this->taps.clear();
+		this->max_delay_tap = 0;
+		for (auto &c: new_config) {
+			EchoTap tap;
+			tap.config = c;
+			this->taps.push_back(tap);
+			this->max_delay_tap = this->max_delay_tap > c.delay ? this->max_delay_tap : c.delay;
+		}
+	}
+
+	if (output_channels != 2) {
+		if (will_crossfade) {
+			this->runEffectInternal<true, false>(&working_buf[0], gain);
+		} else {
+			this->runEffectInternal<false, false>(&working_buf[0], gain);
+		}
+		mixChannels(config::BLOCK_SIZE, &working_buf[0], 2, output, output_channels);
+	} else {
+		if (will_crossfade) {
+			this->runEffectInternal<true, true>(output, gain);
+		} else {
+			this->runEffectInternal<false, true>(output, gain);
+		}
+	}
+}
+
+void EchoEffect::resetEffect() {
+	this->line.clear();
+}
+
+void EchoEffect::pushNewConfig(deferred_vector<EchoTapConfig> &&config) {
+	if (this->pending_configs.enqueue(std::move(config)) == false) {
+		throw std::bad_alloc();
+	}
+}
 
 }
