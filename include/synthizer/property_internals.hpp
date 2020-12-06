e564c6b126ea7e04208cba6417e193babc743cd8
Austin Hicks <ahicks@ahicks.io>
2020-12-06 15:13:41 -0800

    Define classes for specific property types and lift property range macros
    Refs #32

--- include/synthizer/property_internals.hpp
+++ include/synthizer/property_internals.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "synthizer/cells.hpp"
+
 #include <array>
+#include <atomic>
 #include <limits>
 #include <memory>
 #include <variant>
@@ -42,5 +45,77 @@ const auto int_max = std::numeric_limits<int>::max();
 const auto double_min = std::numeric_limits<double>::lowest();
 const auto double_max = std::numeric_limits<double>::max();
 }
+
+/*
+ * these are property containers which orchestrate the storage/retrieval of properties in a threadsafe manner, assuming that
+ * write is only ever called from one thread at a time.
+ * */
+template<typename T>
+class AtomicProperty {
+	public:
+	AtomicProperty(const T&& value): field(value) {}
+
+	T read() { return this->field.load(std::memory_order_acquire); }
+	void write(T value) { this->field.store(T, std::memory_order_release); }
+	private:
+	std::atomic<T> field{};
+};
+
+using IntProperty = AtomicProperty<int>;
+using DoubleProperty = AtomicProperty<double>;
+
+template<typename T>
+class LatchProperty {
+	LatchProperty(const T&& value): field(value) {}
+
+	T read() { return this->field.read(); }
+	void write(const T&& value) { this->field.write(value); }
+
+	private:
+	LatchCell<T> field;
+};
+
+using Double3Property = LatchProperty<std::array<double, 3>>;
+using Double6Property = LatchProperty<std::array<double, 6>>;
+
+template<typename T>
+class ObjectProperty {
+	public:
+	std::weak_ptr<T> read() {
+		std::weak_ptr<T> out;
+		this->lock();
+		out = this->field;
+		this->unlock();
+		return out;
+	}
+
+	void write(const std::weak_ptr<T> &value) {
+		this->lock();
+		this->field = value;
+		this->unlock();
+	}
+
+	void lock() {
+		int old = 0;
+		while (this->spinlock.compare_exchange_strong(old, 1, std::memory_order_acquire, std::memory_order_relaxed) != true) {
+			old = 0;
+			/* Don't yield because this may be the audio thread. */
+		}
+	}
+
+	void unlock() {
+		this->spinlock.store(0, std::memory_order_release);
+	}
+
+	private:
+	std::atomic<int> spinlock = 0;
+	std::weak_ptr<T> field{};
+};
+
+/* Used when expanding the X-lists. */
+#define P_INT_MIN property_impl::int_min
+#define P_INT_MAX property_impl::int_max
+#define P_DOUBLE_MIN property_impl::double_min
+#define P_DOUBLE_MAX property_impl::double_max
 
 }
