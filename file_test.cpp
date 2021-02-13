88c5f510e82b60d46edbe0de469f84d20fa43a60
Austin Hicks <ahicks@ahicks.io>
2021-02-13 14:11:45 -0800

    Bring in file_test.cpp from better-hrtf for listening tests

--- file_test.cpp
+++ file_test.cpp
@@ -28,7 +28,7 @@ struct syz_RouteConfig route_config = { 1.0, 0.01, };
 int main(int argc, char *argv[]) {
 	syz_Handle context = 0, generator = 0, source = 0, buffer = 0, effect = 0;
 	int ecode = 0, ending = 0;
-	double angle = 0.0, angle_per_second = 90.0;
+	double angle = 0.0, angle_per_second = 45.0;
 
 	if (argc != 2) {
 		printf("Usage: wav_test <path>\n");
@@ -40,17 +40,19 @@ int main(int argc, char *argv[]) {
 	CHECKED(syz_initialize());
 
 	CHECKED(syz_createContext(&context));
+	CHECKED(syz_setI(context, SYZ_P_PANNER_STRATEGY, SYZ_PANNER_STRATEGY_HRTF));
+
 	CHECKED(syz_contextEnableEvents(context));
 	CHECKED(syz_createSource3D(&source, context));
 	CHECKED(syz_createBufferFromStream(&buffer, "file", argv[1], ""));
 	CHECKED(syz_createBufferGenerator(&generator, context));
-	//CHECKED(syz_setI(generator, SYZ_P_LOOPING, 1));
-	CHECKED(syz_setD(generator, SYZ_P_PITCH_BEND, 2.0));
+	CHECKED(syz_setI(generator, SYZ_P_LOOPING, 1));
+	//CHECKED(syz_setD(generator, SYZ_P_PITCH_BEND, 2.0));
 	CHECKED(syz_setO(generator, SYZ_P_BUFFER, buffer));
 	CHECKED(syz_sourceAddGenerator(source, generator));
 
 	CHECKED(syz_createGlobalFdnReverb(&effect, context));
-	CHECKED(syz_routingConfigRoute(context, source, effect, &route_config));
+	//CHECKED(syz_routingConfigRoute(context, source, effect, &route_config));
 
 	//std::this_thread::sleep_for(std::chrono::seconds(2));
 	//CHECKED(syz_pause(source));
@@ -70,9 +72,6 @@ int main(int argc, char *argv[]) {
 		CHECKED(syz_setD3(source, SYZ_P_POSITION, x, y, 0.0));
 
 		CHECKED(syz_contextGetNextEvent(&event, context));
-		if (event.type != SYZ_EVENT_TYPE_INVALID) {
-			printf("%u %llu %llu\n", event.type, event.source, event.context);
-		}
 	}
 
 end:
