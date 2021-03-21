1f1c3b348d806d9b532dbed01d33fa3c951a3ed7
Austin Hicks <ahicks@ahicks.io>
2021-03-21 16:16:31 -0700

    Conditionally enable usage of the filesystem header
    On GCC<9, this gets weird enough that we can just not use it, since Linux paths
    are arbitrary bytes.  Unicode will still just work for those platforms.
    
    While we could hardcode the gcc versions, let's also provide a useful fallback
    for other platforms that may not have this support; compilers are better at
    language features than compiler features.
    
    Refs #57

--- src/streams/file.cpp
+++ src/streams/file.cpp
@@ -1,11 +1,19 @@
-#include <filesystem>
+#include "synthizer/byte_stream.hpp"
+
 #include <fstream>
 #include <utility>
 #include <vector>
 #include <tuple>
 #include <memory>
 
-#include "synthizer/byte_stream.hpp"
+/*
+ * Filesystem is flaky on older versions of GCC, etc. In those cases CMake will tell Synthizer
+ * to use the pre-C++17 fallback of a string path, which might break unicode on Windows
+ * Since we require very recent MSVC on Windows, however, that's fine: unicode will still work there.
+ * */
+#ifdef SYZ_USE_FILESYSTEM
+#include <filesystem>
+#endif
 
 namespace synthizer {
 
@@ -73,17 +81,25 @@ std::size_t FileByteStream::getLength() {
 }
 
 std::shared_ptr<ByteStream> fileStream(const std::string &path, const std::vector<std::tuple<std::string, std::string>> &options) {
-	(void) options;
+	std::fstream stream;
 
+#ifdef SYZ_USE_FILESYSTEM
 	auto p = std::filesystem::u8path(path);
+	stream.open(p, std::ios::in|std::ios::binary);
 
 	if (std::filesystem::exists(p) == false)
 		throw EByteStreamNotFound();
 
-	std::fstream stream;
 	stream.open(p, std::ios::in|std::ios::binary);
+#else
+	stream.open(path.c_str(), std::ios::in|std::ios::binary);
+	/*
+	 * This path doesn't have a good way to know if the open failed due to the file not
+	 * existing; we'll just have to throw the generic error below.
+	 * */
+#endif
 	if (stream.is_open() == false)
-		throw EByteStream("File path existed, but we were unable to open.");
+		throw EByteStream("Unable to open file");
 	return std::make_shared<FileByteStream>(std::move(stream));
 }
 
