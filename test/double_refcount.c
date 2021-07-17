337ecaf0ac2bf96e7f6f555b4eeadbc723e91cd6
Austin Hicks <ahicks@ahicks.io>
2021-07-16 17:54:34 -0700

    Add a test that the library doesn't crash when refcounts are incremented and decremented
    It looked like the Rust bindings ofund a bug hree, but so far that doesn't
    seem to be the case.

---
+++ test/double_refcount.c
@@ -0,0 +1,21 @@
+/**
+ * Test that two calls to syz_incRef followed by 2 calls to syz_decRef don't
+ * crash etc.
+ * */
+#include <stdio.h>
+
+#include "synthizer.h"
+
+#define CHECK(X) if ((X) != 0) { printf("Failed: " #X); return 1; }
+
+int main() {
+	syz_Handle h;
+
+	CHECK(syz_initialize());
+	CHECK(syz_createContext(&h, NULL, NULL));
+	CHECK(syz_handleIncRef(h));
+	CHECK(syz_handleDecRef(h));
+	CHECK(syz_handleDecRef(h));
+	CHECK(syz_shutdown());
+	return 0;
+}
