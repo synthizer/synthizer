c5af143afaa9179bdd5fa4d6c17a024bf187caf8
Austin Hicks <ahicks@ahicks.io>
2022-04-09 14:43:15 -0700

    Julia: add a delay line module

---
+++ data_processor_julia/src/delay_line.jl
@@ -0,0 +1,45 @@
+"""
+A simple delay line, supporting adding samples and reading into the past.
+"""
+module DelayLine
+
+mutable struct Line{Channels}
+    data::Matrix{Float64}
+    pos::UInt64
+
+    function Line(::Val{Channels}, length)::DelayLine{Channels} where {Channels}
+        data = zeros(Float64, Channels, length)
+        new{Channels}(data, UInt64(1))
+    end
+end
+
+"""
+Read this delay line, at pos + relative_pos.  Relative_pos is usually negative to read into the past.
+
+The default value for relative_pos, 1, means that a write call followed by a read call will read the value of the write
+call because the write call just advanced.
+"""
+function read(
+    line::Line{Channels},
+    relative_pos::UInt64 = UInt64(1),
+)::NTuple{Channels, Float64} where {Channels}
+    pos = convert(Int64, line.pos) - convert(Int64, relative_pos)
+    pos = mod1(pos, size(line.data)[2])
+    @assert 1 <= pos <= size(line.data)[2]
+    ntuple(Val(Channels)) do i
+        line.data[i, pos]
+    end
+end
+
+"""
+Write this delay line.  Always at the current position.  Advances the position by one.
+"""
+function write(line::Line{Channels}, vals::NTuple{Channels, Float64}) where {Channels}
+    for i = 1:Channels
+        line.data[i, line.pos] = vals[i]
+    end
+
+    line.pos = mod1(line.pos + 1, size(line.data)[2])
+end
+
+end
--- data_processor_julia/src/main.jl
+++ data_processor_julia/src/main.jl
@@ -1,8 +1,12 @@
 include("./hrtf_dataset.jl")
 include("./minimum_phase.jl")
 include("./filter_processing.jl")
+include("./delay_line.jl")
+import .DelayLine as d
 
-dataset = HrtfDataset.load_mit("$(@__DIR__)/../mit_kemar")
-dataset = HrtfDataset.map_to_dataset(dataset) do x
-    x
+line = d.Line(Val{2}(), UInt64(4))
+
+for i = 1:4
+    d.write(line, (Float64(i), Float64(i * 2)))
+    print(d.read(line, UInt64(2)))
 end
