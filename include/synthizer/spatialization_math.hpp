fcfbb5c8453a1857b4f6128aa1390d861b985460
Austin Hicks <ahicks@ahicks.io>
2022-08-13 16:09:33 -0700

    Make the spatialization math helpers header only

--- include/synthizer/spatialization_math.hpp
+++ include/synthizer/spatialization_math.hpp
@@ -2,6 +2,9 @@
 
 #include "synthizer_constants.h"
 
+#include "synthizer/error.hpp"
+#include "synthizer/math.hpp"
+
 #include <array>
 #include <cmath>
 
@@ -68,7 +71,49 @@ template <typename T> void setPropertiesFromDistanceParams(T *dest, const Distan
   dest->setDistanceModel((int)params.distance_model);
 }
 
-double mulFromDistanceParams(const DistanceParams &params);
+inline double mulFromDistanceParams(const DistanceParams &params) {
+  double dist_mul = 1.0;
+
+  switch (params.distance_model) {
+  case SYZ_DISTANCE_MODEL_NONE:
+    dist_mul = 1.0;
+    break;
+  case SYZ_DISTANCE_MODEL_LINEAR:
+    dist_mul = 1 - params.rolloff *
+                       (clamp(params.distance, params.distance_ref, params.distance_max) - params.distance_ref) /
+                       (params.distance_max - params.distance_ref);
+    break;
+  case SYZ_DISTANCE_MODEL_EXPONENTIAL:
+    if (params.distance_ref == 0.0) {
+      dist_mul = 0.0;
+    } else {
+      dist_mul = std::pow(std::max(params.distance, params.distance_ref) / params.distance_ref, -params.rolloff);
+    }
+    break;
+  case SYZ_DISTANCE_MODEL_INVERSE:
+    if (params.distance_ref == 0.0) {
+      dist_mul = 0.0;
+    } else {
+      dist_mul =
+          params.distance_ref /
+          (params.distance_ref + params.rolloff * std::max(params.distance, params.distance_ref) - params.distance_ref);
+    }
+    break;
+  default:
+    dist_mul = 1.0;
+  }
+
+  /*
+   * If the distance is further away than closeness_boost, we reduce.
+   *
+   * This is counterintuitive, but makes sure that distances  closer than closeness_boost don't break attenuation.
+   * */
+  if (params.distance > params.closeness_boost_distance) {
+    dist_mul *= dbToGain(-params.closeness_boost);
+  }
+
+  return clamp(dist_mul, 0.0, 1.0);
+}
 
 /*
  * Helper class which can be used to augment anything with a distance model, with Synthizer-compatible property
@@ -119,6 +164,43 @@ inline Vec3d normalize(const Vec3d &x) {
   return {x[0] / m, x[1] / m, x[2] / m};
 }
 
-void throwIfParallel(const Vec3d &a, const Vec3d &b);
+inline void throwIfParallel(const Vec3d &a, const Vec3d &b) {
+  double dp = dotProduct(a, b);
+  if (dp > 0.95)
+    throw EInvariant("Vectors must not be parallel");
+}
+
+inline DistanceParams &DistanceParamsMixin::getDistanceParams() { return this->distance_params; }
+
+inline void DistanceParamsMixin::setDistanceParams(const DistanceParams &params) { this->distance_params = params; }
+
+inline double DistanceParamsMixin::getDistanceRef() { return this->distance_params.distance_ref; }
+
+inline void DistanceParamsMixin::setDistanceRef(double val) { this->distance_params.distance_ref = val; }
+
+inline double DistanceParamsMixin::getDistanceMax() { return this->distance_params.distance_max; }
+
+inline void DistanceParamsMixin::setDistanceMax(double val) { this->distance_params.distance_max = val; }
+
+inline double DistanceParamsMixin::getRolloff() { return this->distance_params.rolloff; }
+
+inline void DistanceParamsMixin::setRolloff(double val) { this->distance_params.rolloff = val; }
+inline double DistanceParamsMixin::getClosenessBoost() { return this->distance_params.closeness_boost; }
+
+inline void DistanceParamsMixin::setClosenessBoost(double val) { this->distance_params.closeness_boost = val; }
+
+inline double DistanceParamsMixin::getClosenessBoostDistance() {
+  return this->distance_params.closeness_boost_distance;
+}
+
+inline void DistanceParamsMixin::setClosenessBoostDistance(double val) {
+  this->distance_params.closeness_boost_distance = val;
+}
+
+inline int DistanceParamsMixin::getDistanceModel() { return this->distance_params.distance_model; }
+
+inline void DistanceParamsMixin::setDistanceModel(int val) {
+  this->distance_params.distance_model = (enum SYZ_DISTANCE_MODEL)val;
+}
 
 } // namespace synthizer
