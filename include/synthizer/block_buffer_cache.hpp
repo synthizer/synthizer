c280ec6a56abb4d730a3d95166b4e7182d7162e1
Austin Hicks <ahicks@ahicks.io>
2022-08-14 14:05:25 -0700

    Make the block buffer cache header only

--- include/synthizer/block_buffer_cache.hpp
+++ include/synthizer/block_buffer_cache.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "synthizer/config.hpp"
+#include "synthizer/memory.hpp"
 #include "synthizer/trylock.hpp"
 
+#include <array>
 #include <cstdint>
+#include <cstdlib>
 
 namespace synthizer {
 
@@ -49,4 +52,64 @@ private:
  * May allocate if the cache overflow.
  * */
 BlockBufferGuard acquireBlockBuffer(bool should_zero = true);
+
+namespace bbc_detail {
+
+class BlockBufferCache {
+public:
+  BlockBufferCache() {
+    this->count = 3;
+    for (std::size_t i = 0; i < this->count; i++) {
+      this->entries[i] = (float *)std::calloc(config::BLOCK_SIZE * config::MAX_CHANNELS, sizeof(float));
+    }
+  }
+
+  /* It's internally a stack. */
+  std::array<float *, MAX_BLOCK_BUFFER_CACHE_ENTRIES> entries = {{nullptr}};
+  std::size_t count = 0;
+};
+
+inline TryLock<BlockBufferCache> block_buffer_cache{};
+} // namespace bbc_detail
+
+inline BlockBufferGuard acquireBlockBuffer(bool should_zero) {
+  using namespace bbc_detail;
+
+  float *out = nullptr;
+
+  block_buffer_cache.withLock([&](auto *cache) {
+    if (cache->count == 0) {
+      /* Handle allocation at the end of the function, outside the lock. */
+      return;
+    }
+    cache->count -= 1;
+    out = cache->entries[cache->count];
+  });
+
+  if (out == nullptr) {
+    out = (float *)std::calloc(config::BLOCK_SIZE * config::MAX_CHANNELS, sizeof(float));
+  } else if (should_zero) {
+    std::fill(out, out + config::BLOCK_SIZE * config::MAX_CHANNELS, 0.0f);
+  }
+
+  return BlockBufferGuard(out);
+}
+
+inline BlockBufferGuard::~BlockBufferGuard() {
+  using namespace bbc_detail;
+
+  block_buffer_cache.withLock([&](auto *cache) {
+    if (cache->count == cache->entries.size()) {
+      return;
+    }
+    cache->entries[cache->count] = this->data;
+    cache->count++;
+    /* Give the cache ownership. */
+    this->data = nullptr;
+  });
+  if (this->data != nullptr) {
+    deferredFree(this->data);
+  }
+}
+
 } // namespace synthizer
