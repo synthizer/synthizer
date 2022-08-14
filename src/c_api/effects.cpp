3ef2f1a40f7d3b1fdcd9b8e9c24ae84904ff5473
Austin Hicks <ahicks@ahicks.io>
2022-08-14 15:19:43 -0700

    Centralize the C API for effects and get rid of the empty TUs

--- src/c_api/effects.cpp
+++ src/c_api/effects.cpp
@@ -4,12 +4,25 @@
 #include "synthizer/effects/echo.hpp"
 
 #include "synthizer/c_api.hpp"
+#include "synthizer/effects/base_effect.hpp"
+#include "synthizer/effects/fdn_reverb.hpp"
 #include "synthizer/effects/global_effect.hpp"
 #include "synthizer/error.hpp"
 
 #include <utility>
 
 using namespace synthizer;
+
+SYZ_CAPI syz_ErrorCode syz_effectReset(syz_Handle effect) {
+  SYZ_PROLOGUE
+  auto e = fromC<BaseObject>(effect);
+  auto e_as_effect = fromC<BaseEffect>(effect);
+  Context *ctx = e->getContextRaw();
+  ctx->enqueueReferencingCallbackCommand(
+      true, [](auto &e_as_effect) { e_as_effect->resetEffect(); }, e_as_effect);
+  return 0;
+  SYZ_EPILOGUE
+}
 
 SYZ_CAPI syz_ErrorCode syz_createGlobalEcho(syz_Handle *out, syz_Handle context, void *config, void *userdata,
                                             syz_UserdataFreeCallback *userdata_free_callback) {
@@ -43,5 +56,18 @@ SYZ_CAPI syz_ErrorCode syz_globalEchoSetTaps(syz_Handle handle, unsigned int n_t
   }
   echo->pushNewConfig(std::move(cfg));
   return 0;
+  SYZ_EPILOGUE
+}
+
+SYZ_CAPI syz_ErrorCode syz_createGlobalFdnReverb(syz_Handle *out, syz_Handle context, void *config, void *userdata,
+                                                 syz_UserdataFreeCallback *userdata_free_callback) {
+  SYZ_PROLOGUE(void) config;
+
+  auto ctx = fromC<Context>(context);
+  auto x = ctx->createObject<GlobalFdnReverbEffect>();
+  std::shared_ptr<GlobalEffect> e = x;
+  ctx->registerGlobalEffect(e);
+  *out = toC(x);
+  return syz_handleSetUserdata(*out, userdata, userdata_free_callback);
   SYZ_EPILOGUE
 }
