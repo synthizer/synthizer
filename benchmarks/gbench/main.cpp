6a5105e4d05d5ee4c9d45beec88ba05c5434e211
Austin Hicks <ahicks@ahicks.io>
2022-08-16 16:28:55 -0700

    Write our own google/benchmark main function

---
+++ benchmarks/gbench/main.cpp
@@ -0,0 +1,10 @@
+#include <benchmark/benchmark.h>
+
+int main(int argc, char **argv) {
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv))
+    return 1;
+  ::benchmark::RunSpecifiedBenchmarks();
+  ::benchmark::Shutdown();
+  return 0;
+}
