dcfbdffb6c473467436c47a453e71ae93ebe347a
Austin Hicks <ahicks@ahicks.io>
2022-08-28 18:03:16 -0700

    ModPointer: add an asserting interface to replace raw pointers in debug builds
    This is opt-in via an extra parameter to createXXXModPointer because some things like the HRTF
    actually rely on raw pointers being passed for, e.g., SIMD overloads.  When opted in, a debug build
    (NDEBUG not defined) will never dispatch a raw pointer for the returned ModPointer and
    every access will assert that it is in bounds.

--- include/synthizer/mod_pointer.hpp
+++ include/synthizer/mod_pointer.hpp
@@ -6,6 +6,11 @@
  * while transparently performing modulus, or a raw pointer itself.  This can then be used either directly by writing
  * template functions or indirectly by writing overloads which capture pointers to some type T and doing whatever with
  * them.
+ *
+ * In debug builds, the raw pointers can be replaced with an asserting version that can detect bounds checks.  Because
+ * some things actually hard rely on the raw pointer being available, this is off by default and must be passed as an
+ * additional parameter to the various createXXX functions below.  In a release build (specifically when NDEBUG is
+ * defined), this is a no-op.
  * */
 #pragma once
 
@@ -18,25 +23,50 @@
 namespace synthizer {
 
 namespace mod_pointer_detail {
-/**
- * A length provider based off a static compile-time-known len.
- * */
-template <std::size_t LEN> class StaticLengthProvider {
+template <std::size_t LEN> class StaticModProvider {
 public:
   std::size_t getLength() const { return LEN; }
+  std::size_t doMod(std::size_t val) const { return val % LEN; }
 };
 
-/**
- * A dynamic length provider, where the length is passed in by createDynamicModPointer.
- * */
-class DynamicLengthProvider {
+class DynamicModProvider {
 public:
-  DynamicLengthProvider(std::size_t len) : length(len) {}
+  DynamicModProvider(std::size_t len) : length(len) {}
 
   std::size_t getLength() const { return this->length; }
+  std::size_t doMod(std::size_t val) const { return val % this->length; }
 
 private:
   std::size_t length;
+};
+
+/**
+ * A static modulus provider which asserts that modulus never happens.
+ * */
+template <std::size_t LEN> class StaticAssertingProvider {
+public:
+  std::size_t getLength() const { return LEN; }
+  std::size_t doMod(std::size_t val) const {
+    assert(val < LEN);
+    return val;
+  }
+};
+
+/**
+ * A dynamic modulus provider which asserts that modulus never happens.
+ * */
+class DynamicAssertingProvider {
+public:
+  DynamicAssertingProvider(std::size_t _len) : length(_len) {}
+
+  std::size_t getLength() const { return this->length; }
+  std::size_t doMod(std::size_t val) const {
+    assert(val < this->length);
+    return val;
+  }
+
+private:
+  std::size_t length = 0;
 };
 } // namespace mod_pointer_detail
 
@@ -52,20 +82,20 @@ private:
  * This is, in essence, a "pointer", and being outside the range is the same as out of bounds access.  One difference
  * which hopefully never matters is that, for our sanity, we don't support negative indices.
  * */
-template <typename T, typename LEN_PROVIDER_T> class ModSlice {
+template <typename T, typename MOD_PROVIDER_T> class ModSlice {
 public:
-  ModSlice(T *_data, std::size_t initial_offset, LEN_PROVIDER_T length_provider);
+  ModSlice(T *_data, std::size_t initial_offset, MOD_PROVIDER_T mod_provider);
 
   T &operator[](std::size_t index) const;
   T &operator*() const;
   T *operator->() const;
 
-  ModSlice<T, LEN_PROVIDER_T> &operator++();
-  ModSlice<T, LEN_PROVIDER_T> &operator--();
-  ModSlice<T, LEN_PROVIDER_T> operator++(int);
-  ModSlice<T, LEN_PROVIDER_T> operator--(int);
-  ModSlice<T, LEN_PROVIDER_T> operator+(std::size_t increment) const;
-  ModSlice<T, LEN_PROVIDER_T> operator-(std::size_t decrement) const;
+  ModSlice<T, MOD_PROVIDER_T> &operator++();
+  ModSlice<T, MOD_PROVIDER_T> &operator--();
+  ModSlice<T, MOD_PROVIDER_T> operator++(int);
+  ModSlice<T, MOD_PROVIDER_T> operator--(int);
+  ModSlice<T, MOD_PROVIDER_T> operator+(std::size_t increment) const;
+  ModSlice<T, MOD_PROVIDER_T> operator-(std::size_t decrement) const;
   void operator+=(std::size_t increment);
 
 private:
@@ -81,7 +111,7 @@ private:
 
   T *data;
   std::size_t offset;
-  LEN_PROVIDER_T length_provider;
+  MOD_PROVIDER_T mod_provider;
 };
 
 /**
@@ -96,7 +126,11 @@ private:
  * helper function for construction of ModPointers.
  * */
 template <typename T, std::size_t LEN>
-using StaticModPointer = std::variant<ModSlice<T, mod_pointer_detail::StaticLengthProvider<LEN>>, T *>;
+using StaticModPointer = std::variant<ModSlice<T, mod_pointer_detail::StaticModProvider<LEN>>,
+#ifndef NDEBUG
+                                      ModSlice<T, mod_pointer_detail::StaticAssertingProvider<LEN>>,
+#endif
+                                      T *>;
 
 /**
  * The same as StaticModPointer, but the modulus case comes from a runtime value.
@@ -104,109 +138,128 @@ using StaticModPointer = std::variant<ModSlice<T, mod_pointer_detail::StaticLeng
  * This is significantly more expensive in most contexts.
  * */
 template <typename T>
-using DynamicModPointer = std::variant<ModSlice<T, mod_pointer_detail::DynamicLengthProvider>, T *>;
+using DynamicModPointer = std::variant<ModSlice<T, mod_pointer_detail::DynamicModProvider>,
+#ifndef NDEBUG
+                                       ModSlice<T, mod_pointer_detail::DynamicAssertingProvider>,
+#endif
+                                       T *>;
 
 /**
  * Given a pointer, an offset, and one past the maximum index which may be accessed, return a ModPointer that can handle
  * it.
  * */
 template <typename T, std::size_t LEN>
-inline StaticModPointer<T, LEN> createStaticModPointer(T *data, std::size_t offset, std::size_t slice_len);
+inline StaticModPointer<T, LEN> createStaticModPointer(T *data, std::size_t offset, std::size_t slice_len,
+                                                       bool allow_asserting_provider = false);
 
 template <typename T>
-inline DynamicModPointer<T> createStaticModPointer(T *data, std::size_t offset, std::size_t max_index,
-                                                   std::size_t buffer_len);
+inline DynamicModPointer<T> createDynamicModPointer(T *data, std::size_t offset, std::size_t max_index,
+                                                    std::size_t buffer_len, bool allow_asserting_provider = false);
 
-template <typename T, typename LEN_PROVIDER_T>
-ModSlice<T, LEN_PROVIDER_T>::ModSlice(T *_data, std::size_t initial_offset, LEN_PROVIDER_T _length_provider)
-    : data(_data), offset(initial_offset), length_provider(_length_provider) {}
+template <typename T, typename MOD_PROVIDER_T>
+ModSlice<T, MOD_PROVIDER_T>::ModSlice(T *_data, std::size_t initial_offset, MOD_PROVIDER_T _length_provider)
+    : data(_data), offset(initial_offset), mod_provider(_length_provider) {}
 
-template <typename T, typename LEN_PROVIDER_T>
-FLATTENED std::size_t ModSlice<T, LEN_PROVIDER_T>::addIndexRelative(std::size_t increment) const {
-  return (this->offset + increment) % this->length_provider.getLength();
+template <typename T, typename MOD_PROVIDER_T>
+FLATTENED std::size_t ModSlice<T, MOD_PROVIDER_T>::addIndexRelative(std::size_t increment) const {
+  return this->mod_provider.doMod(this->offset + increment);
 }
 
-template <typename T, typename LEN_PROVIDER_T>
-FLATTENED std::size_t ModSlice<T, LEN_PROVIDER_T>::subIndexRelative(std::size_t decrement) const {
-  assert(decrement <= this->length_provider.getLength());
-  return (this->length_provider.getLength() + this->offset - decrement) % this->length_provider.getLength();
+template <typename T, typename MOD_PROVIDER_T>
+FLATTENED std::size_t ModSlice<T, MOD_PROVIDER_T>::subIndexRelative(std::size_t decrement) const {
+  assert(decrement <= this->mod_provider.getLength());
+  return this->mod_provider.doMod(this->mod_provider.getLength() + this->offset - decrement);
 }
 
-template <typename T, typename LEN_PROVIDER_T>
-FLATTENED T &ModSlice<T, LEN_PROVIDER_T>::operator[](std::size_t index) const {
+template <typename T, typename MOD_PROVIDER_T>
+FLATTENED T &ModSlice<T, MOD_PROVIDER_T>::operator[](std::size_t index) const {
   auto actual_index = this->addIndexRelative(index);
   return this->data[actual_index];
 }
 
-template <typename T, typename LEN_PROVIDER_T> FLATTENED T &ModSlice<T, LEN_PROVIDER_T>::operator*() const {
+template <typename T, typename MOD_PROVIDER_T> FLATTENED T &ModSlice<T, MOD_PROVIDER_T>::operator*() const {
   return (*this)[0];
 }
 
-template <typename T, typename LEN_PROVIDER_T> T *ModSlice<T, LEN_PROVIDER_T>::operator->() const {
+template <typename T, typename MOD_PROVIDER_T> T *ModSlice<T, MOD_PROVIDER_T>::operator->() const {
   return &((*this)[0]);
 }
 
-template <typename T, typename LEN_PROVIDER_T> ModSlice<T, LEN_PROVIDER_T> &ModSlice<T, LEN_PROVIDER_T>::operator++() {
+template <typename T, typename MOD_PROVIDER_T> ModSlice<T, MOD_PROVIDER_T> &ModSlice<T, MOD_PROVIDER_T>::operator++() {
   this->offset = this->addIndexRelative(1);
   return *this;
 }
 
-template <typename T, typename LEN_PROVIDER_T> ModSlice<T, LEN_PROVIDER_T> &ModSlice<T, LEN_PROVIDER_T>::operator--() {
+template <typename T, typename MOD_PROVIDER_T> ModSlice<T, MOD_PROVIDER_T> &ModSlice<T, MOD_PROVIDER_T>::operator--() {
   // This is equivalent to going around one minus the len.
   this->offset = this->subIndexRelative(1);
   return *this;
 }
 
-template <typename T, typename LEN_PROVIDER_T>
-ModSlice<T, LEN_PROVIDER_T> ModSlice<T, LEN_PROVIDER_T>::operator++(int) {
+template <typename T, typename MOD_PROVIDER_T>
+ModSlice<T, MOD_PROVIDER_T> ModSlice<T, MOD_PROVIDER_T>::operator++(int) {
   auto old = *this;
   ++(*this);
   return old;
 }
 
-template <typename T, typename LEN_PROVIDER_T>
-ModSlice<T, LEN_PROVIDER_T> ModSlice<T, LEN_PROVIDER_T>::operator--(int) {
+template <typename T, typename MOD_PROVIDER_T>
+ModSlice<T, MOD_PROVIDER_T> ModSlice<T, MOD_PROVIDER_T>::operator--(int) {
   auto old = *this;
   --(*this);
   return old;
 }
 
-template <typename T, typename LEN_PROVIDER_T>
-ModSlice<T, LEN_PROVIDER_T> ModSlice<T, LEN_PROVIDER_T>::operator+(std::size_t increment) const {
+template <typename T, typename MOD_PROVIDER_T>
+ModSlice<T, MOD_PROVIDER_T> ModSlice<T, MOD_PROVIDER_T>::operator+(std::size_t increment) const {
   auto copy = *this;
   copy.offset = this->addIndexRelative(increment);
   return copy;
 }
 
-template <typename T, typename LEN_PROVIDER_T>
-ModSlice<T, LEN_PROVIDER_T> ModSlice<T, LEN_PROVIDER_T>::operator-(std::size_t decrement) const {
+template <typename T, typename MOD_PROVIDER_T>
+ModSlice<T, MOD_PROVIDER_T> ModSlice<T, MOD_PROVIDER_T>::operator-(std::size_t decrement) const {
   auto copy = *this;
   copy.offset = this->subIndexRelative(decrement);
   return copy;
 }
 
-template <typename T, typename LEN_PROVIDER_T>
-FLATTENED void ModSlice<T, LEN_PROVIDER_T>::operator+=(std::size_t increment) {
+template <typename T, typename MOD_PROVIDER_T>
+FLATTENED void ModSlice<T, MOD_PROVIDER_T>::operator+=(std::size_t increment) {
   *this = *this + increment;
 }
 
 template <typename T, std::size_t LEN>
-FLATTENED StaticModPointer<T, LEN> createStaticModPointer(T *data, std::size_t offset, std::size_t slice_len) {
+FLATTENED StaticModPointer<T, LEN> createStaticModPointer(T *data, std::size_t offset, std::size_t slice_len,
+                                                          bool allow_asserting_provider) {
   if (offset + slice_len > LEN) {
-    return ModSlice<T, mod_pointer_detail::StaticLengthProvider<LEN>>{data, offset,
-                                                                      mod_pointer_detail::StaticLengthProvider<LEN>{}};
+    return ModSlice<T, mod_pointer_detail::StaticModProvider<LEN>>{data, offset,
+                                                                   mod_pointer_detail::StaticModProvider<LEN>{}};
   } else {
+#ifndef NDEBUG
+    if (allow_asserting_provider == true) {
+      return ModSlice<T, mod_pointer_detail::StaticAssertingProvider<LEN>>{
+          data, offset, mod_pointer_detail::StaticAssertingProvider<LEN>{}};
+    }
+#endif
+
     return data + offset;
   }
 }
 
 template <typename T>
 FLATTENED DynamicModPointer<T> createDynamicModPointer(T *data, std::size_t offset, std::size_t slice_len,
-                                                       std::size_t buffer_len) {
+                                                       std::size_t buffer_len, bool allow_asserting_provider) {
   if (offset + slice_len > buffer_len) {
-    return ModSlice<T, mod_pointer_detail::DynamicLengthProvider>{
-        data, offset, mod_pointer_detail::DynamicLengthProvider{buffer_len}};
+    return ModSlice<T, mod_pointer_detail::DynamicModProvider>{data, offset,
+                                                               mod_pointer_detail::DynamicModProvider{buffer_len}};
   } else {
+#ifndef NDEBUG
+    if (allow_asserting_provider) {
+      return ModSlice<T, mod_pointer_detail::DynamicAssertingProvider>{
+          data, offset, mod_pointer_detail::DynamicAssertingProvider{buffer_len}};
+    }
+#endif
     return data + offset;
   }
 }
